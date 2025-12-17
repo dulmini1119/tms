fbd44c4d490412f5ab87f7a0bb9cf7e0d484acda
dulmini1119 <senewirathnapabodha@gmail.com>
2025-12-17 14:59:45 +0530

    Initial Commit

---
+++ backend/src/modules/vehicles/dto/update-vehicle.dto.ts
@@ -0,0 +1,12 @@
+export interface UpdateVehicleDto {
+  make?: string;
+  model?: string;
+  year?: number;
+  color?: string;
+
+  operational_status?: string;
+  availability_status?: string;
+
+  current_driver_id?: string;
+  assigned_department_id?: string;
+}
